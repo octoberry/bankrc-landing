2831b59edb11464059a6194194bd59308e0dca7f
mr.korsar <mr.blackheart1991@gmail.com>
2014-09-27 04:08:05 +0400

    NonameBank v2.0

---
+++ nnmbank/old/js/main.js
@@ -0,0 +1,191 @@
+$(document).ready(function () {
+    
+    if (device.mobile()) {
+        $("body").addClass("mobile");
+        
+        if (device.landscape()) {
+            $("body").addClass("landscape");
+            $("body").removeClass("not-landscape");
+        } else {
+            $("body").removeClass("landscape");
+            $("body").addClass("not-landscape");
+        }
+    }
+    
+    setBorderPage();
+    $(window).resize(function () {
+        setBorderPage();
+    });
+    $('body').bind('orientationchange', function () {
+        setBorderPage();
+    });
+
+    $('#fullpage').fullpage({
+        verticalCentered: true,
+        resize: false,
+        sectionsColor: ['white'],
+        anchors: [],
+        scrollingSpeed: 700,
+        easing: 'fadeIn',
+        menu: false,
+        navigation: true,
+        navigationPosition: 'right',
+        slidesNavigation: false,
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: false,
+        autoScrolling: true,
+        scrollOverflow: false,
+        css3: true,
+        paddingTop: '0px',
+        paddingBottom: '0px',
+        normalScrollElements: '',
+        normalScrollElementTouchThreshold: 5,
+        keyboardScrolling: false,
+        touchSensitivity: 15,
+        continuousVertical: false,
+        animateAnchor: true,
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+        //events
+        onLeave: function (index, nextIndex, direction) {
+            $.fn.fullpage.moveTo(nextIndex, 0);
+            if (nextIndex == 4 || index == 4){
+                $(".btn-fix").fadeOut();
+            } else {
+                $(".btn-fix").fadeIn();
+            }
+            setBorderPage();
+        },
+        afterLoad: function (anchorLink, index) {
+        },
+        afterRender: function () {
+        },
+        afterResize: function () {
+        },
+        beforeSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+            if (slideIndex != 0) {
+                $(".btn-fix").fadeOut();
+            }
+        },
+        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
+            if (slideIndex == 0 && index != 4) {
+                $(".btn-fix").fadeIn();
+            }
+        },
+        onSlideLeave: function (anchorLink, index, slideIndex, direction) {
+        }
+    });
+    
+     $('form input').focus(function() {
+       
+    }).blur(function() {
+        setBorderPage();
+        $.fn.fullpage.reBuild();
+    });
+
+    var listRU = $.masksSort($.masksLoad("/js/plugins/inputmask/phones-ru.json"), ['#'], /[0-9]|#/, "mask");
+    var optsRU = {
+        inputmask: {
+            definitions: {
+                '#': {validator: "[0-9]", cardinality: 2}
+            },
+            showMaskOnHover: false, autoUnmask: false, clearMaskOnLostFocus: true, clearIncomplete: true, placeholder: "_"
+        },
+        match: /[0-9]/, replace: '#', list: listRU, listKey: "mask"
+    };
+    //Формализация ввода телефона
+    $('#rf1-phone-field').inputmasks(optsRU);
+    $('#rf2-phone-field').inputmasks(optsRU);
+    $('#rf3-phone-field').inputmasks(optsRU);
+    $('#rf4-phone-field').inputmasks(optsRU);
+    
+    $('#rf1-phone-field, #rf2-phone-field, #rf3-phone-field, #rf4-phone-field').focus(function () {
+        if (!$(this).val()) {
+            $(this).val(7);
+        }
+    });
+    $('#rf1-phone-field, #rf2-phone-field, #rf3-phone-field, #rf4-phone-field').blur(function () {
+        var str = $(this).val();
+        var count = 0;
+        for (var i = 0; i < str.length; i++) {
+            if (/\d/.test(str[i])) count++;
+        }
+        if (count == 1) {
+            $(this).val("");
+        }
+    });
+    $('input[name=phone]').on("keypress", function (key) {
+        if (key.charCode == 13)
+            return true;
+        if (key.charCode < 48 || key.charCode > 57)
+            return false;
+    });
+    
+    $("*[data-id=feature-5]").change(function(){
+        var $this = $(this);
+        var _this = this;
+        var form_prefix = $(this).attr("data-form-prefix");
+        if (_this.checked) {
+            $("#" + form_prefix + "-feature-my-block").removeClass("disabled");
+            $("#" + form_prefix + "-feature-my-field").removeAttr("disabled");
+        } else {
+            $("#" + form_prefix + "-feature-my-block").addClass("disabled");
+            $("#" + form_prefix + "-feature-my-field").attr("disabled", "disabled");
+        }
+    });
+
+
+    //Модальная форма "Оставьте свой телефон"
+    $('*[data-id=request-form]').submit(function () {
+        var form = $(this);
+        var submit = form.find("*[type=submit]");
+        submit.attr("disabled", "disabled");
+        form.append('<input type="hidden" name="klaatu" id="klaatu" value="klaatu_ajax" />');
+        form.attr("action", GLOBAL.URL_POST);
+        var options = {
+            dataType: 'json',
+            beforeSubmit: function () {
+                form.find('.alert, .success, .error').hide();
+                form.find('.has-error').removeClass('has-error');
+            },
+            success: function (data) {
+                submit.removeAttr("disabled");
+                if (data.errors) {
+                    $('#klaatu').remove();
+                    $.each(data.errors, function (i, n) {
+                        if (i == 'required') {
+                            for (var j in n) {
+                                form.find('*[name=' + n[j] + ']').parent().addClass('has-error');
+                            }
+                            //$("#cfm_nodata_error").show();
+                        } else {
+                            //$('#cfm_' + i + '_error').show();
+                        }
+                    });
+                } else {
+                    form.hide();
+                    $('#rf_success_'+form.attr("id").split('-')[2]).fadeIn();
+                    
+                }
+            }
+        };
+
+        $(this).ajaxSubmit(options);
+        return false;
+    });
+});
+
+function formRefresh(){
+    $('*[id^=rf_success').hide();
+    $('*[data-id=request-form]').show();
+    clearForm("*[data-id=request-form]");
+}
+
+function setBorderPage() {
+    var h = $(window).height();
+    if ($("#border-page").length){
+        $("#border-page").css({height: h + "px"});
+        $("#border-page .inner").css({height: (h - (parseInt($("#border-page").css("border-top-width").replace("px", "")) * 2)) + "px"});
+    }
+}
---
+++ nnmbank/old/js/scripts.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+    $('input:checkbox, input:radio').each(function() {
+        var o = $(this);
+        var fc = $('<img/>').width(21).height(22).attr('src', '/css/blank.gif');
+        var id = o.attr('id');
+        if (!id) {
+            id = o.attr('name').replace(/[\[\]]+/g, '');
+            o.attr('id', id);
+        }
+        var fl = $('<label/>').attr('for', id).append(fc).addClass('simpleCheckbox ' + this.type);
+        if (this.checked) {
+            fl.addClass('fChecked');
+        }
+
+        o.data('fCheckbox', fl).after(fl).change(function() {
+            var f = $(this).data('fCheckbox');
+
+            if (this.checked) {
+                f.addClass('fChecked');
+            } else {
+                f.removeClass('fChecked');
+            }
+
+            if (this.type == 'radio') {
+                // это радио, надо выключить остальные кнопки
+                var i = this;
+                $('input[name=' + i.name.replace(/\[+/g, "\\[").replace(/\]+/g, "\\]") + ']').each(function() {
+                    if (this != i) {
+                        $(this).data('fCheckbox').removeClass('fChecked');
+                    }
+                })
+            }
+        }).css({
+            'opacity': 0
+        }).appendTo(fl);
+    });
+});
+
+
+/**
+ * Очищает форму от введенных данных
+ * @param {string} form_id - идентификатор формы
+ */
+function clearForm(form_id) {
+    var form = $(form_id);
+    if (!form.length)
+        return;
+    form.find('.alert, .success, .error').hide();
+    form.find('.has-error').removeClass('has-error');
+    
+    form.find('input[type=text], input[type=file], input[type=email], input[type=password]').each(function() {
+        $(this).val('');
+    });
+    
+    form.find('textarea').each(function() {
+        $(this).val('');
+    });
+    
+    form.find('input[type=checkbox]').each(function() {
+        $(this).removeAttr('checked');
+    });
+    form.find(".fChecked").removeClass("fChecked");
+    
+    form.find('input[type=radio]').each(function() {
+        $(this).removeAttr('checked');
+    });
+    
+    form.find('select').each(function() {
+        $(this).find('option').each(function() {
+            $(this).removeAttr('selected');
+        });
+        $(this).find('option[value=0]').attr('selected', 'selected');
+    });
+}
