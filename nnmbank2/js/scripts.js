18a2382a46decaf075fc3e66c0fadd0dd934f113
mr.korsar <mr.blackheart1991@gmail.com>
2015-02-11 12:25:59 +0400

    NonameBank v3.3.6

--- nnmbank2/js/scripts.js
+++ nnmbank2/js/scripts.js
@@ -64,8 +64,9 @@ $(document).ready(function () {
     $('#glm-phone-field').inputmasks(optsRU);
     $('#glb-phone-field').inputmasks(optsRU);
     $('form').on("keypress", "input[name=phone]", (function(key) {
-        if (key.charCode == 13){
-            return true;
+        if (key.keyCode == 13){
+            $(this).closest("form").submit();
+            return false;
         }
         if (key.charCode < 48 || key.charCode > 57){
             return false;
